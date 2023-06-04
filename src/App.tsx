c057587ee9cb81b792b261e1cbe11900b5b0d347
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-04 17:31:23 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Guest,
+  Home,
+  Inventory,
+  Login,
+  NotFound,
+  Register,
+  Stat,
+  User,
+} from "./pages";
 
 function App() {
   return (
-    <h1>home</h1>
+    <Router>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Guest />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/user/#id" element={<User />} />
+        <Route path="/inventory/#id" element={<Inventory />} />
+        <Route path="/inventory/#id/stats" element={<Stat />} />
+      </Routes>
+    </Router>
   );
 }
 
