babbf7787d4777e8e9ffebef029d2c12431f26ee
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-04 17:27:49 +0200

    export skeleton

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Guest from "./Guest";
+import Home from "./Home";
+import Inventory from "./Inventory";
+import Login from "./Login";
+import NotFound from "./NotFound";
+import Offline from "./Offline";
+import Register from "./Register";
+import Stat from "./Stat";
+import User from "./User";
+
+export {
+  Guest,
+  Home,
+  Inventory,
+  Login,
+  NotFound,
+  Offline,
+  Register,
+  Stat,
+  User,
+};
