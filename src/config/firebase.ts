2641c0ee1b199e4081ed0ad0a473a32398ffb934
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-06 14:47:33 +0200

    add auth

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA8oL4xTKPP-ULaWD9fjZgKJoH8cHWK2Xk",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
