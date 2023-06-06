ffd5a10cfa4470fbd5065b17bb32867a831117ab
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-06 14:24:38 +0200

    add firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8oL4xTKPP-ULaWD9fjZgKJoH8cHWK2Xk",
+  authDomain: "keep-inventory.firebaseapp.com",
+  projectId: "keep-inventory",
+  storageBucket: "keep-inventory.appspot.com",
+  messagingSenderId: "60277865116",
+  appId: "1:60277865116:web:90b5de69fd2bb65a224b59",
+  measurementId: "G-VFEJL12QFE",
+};
+
+const app = initializeApp(firebaseConfig);
