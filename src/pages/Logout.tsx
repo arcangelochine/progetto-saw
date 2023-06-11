1f6964b0c1df6072c0304095ccc0433456002e1d
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-11 22:27:31 +0200

    update Logout

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,6 +1,22 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { logout } from "../config/Auth";
+import { useEffect, useState } from "react";
+import { Spinner } from "../components/utils";
+
 // Pagina per il logout
 const Logout = () => {
-  return <></>;
+  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    logout().then(() => {
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <Spinner />;
+
+  return <Navigate to="/" state={{ from: location }} replace />;
 };
 
 export default Logout;
