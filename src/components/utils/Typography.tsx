15eb72538a9e4920f862a7e3c0568a9edcad6695
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-13 21:14:22 +0200

    update Typography

--- src/components/utils/Typography.tsx
+++ src/components/utils/Typography.tsx
@@ -1,20 +1,31 @@
 import { styled } from "styled-components";
 
 export const Title = styled.h1`
+  font-size: 40px;
   font-weight: bold;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 30px;
+  }
 `;
 
 export const Paragraph = styled.p`
   font-size: 16px;
   font-weight: normal;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 14px;
+  }
 `;
 
 export const Accent = styled.span`
+  font-size: inherit;
   font-weight: inherit;
   color: var(--accent);
 `;
 
 export const Link = styled.a`
+  font-size: inherit;
   font-weight: inherit;
   text-decoration: none;
   color: var(--accent);
