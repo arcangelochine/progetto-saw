fc7d9a7eb6b5975d91a1da763f0dfe0d483626ab
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-14 23:57:22 +0200

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
             }
           />
           <Route
-            path="/user/:id"
+            path="/edit"
             element={
               <PrivateRoute>
                 <User />
