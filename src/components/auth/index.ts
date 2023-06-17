ca542bf1bfa0788b28c11eb07cc82dc74d0ec397
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-17 14:33:08 +0200

    update export

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,6 +1,4 @@
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
-import { Container, LogoAndTitle } from "./Utils";
 
 export { LoginForm, RegisterForm };
-export { Container, LogoAndTitle };
