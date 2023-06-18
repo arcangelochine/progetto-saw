842886a1458843dd8f3ef31bcdb1b50b444bbcdc
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-18 21:53:41 +0200

    update Database interface

--- src/config/Database.ts
+++ src/config/Database.ts
@@ -1,21 +1,24 @@
 import { db } from "./firebase";
-import { inventoryConverter, userConverter } from "../entities";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  Inventory,
+  Item,
+  inventoryConverter,
+  userConverter,
+} from "../entities";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { User } from "firebase/auth";
+
+const DEFAULT_INVENTORY_NAME = "Nuovo inventario";
+const DEFAULT_INVENTORY_AMOUNT = 5;
+const DEFAULT_INVENTORY_CAPACITY = 1000;
+const PREMIUM_INVENTORY_AMOUNT = 10;
+const PREMIUM_INVENTORY_CAPACITY = 5000;
 
 const inventories = collection(db, "inventories").withConverter(
   inventoryConverter
 );
 
 const users = collection(db, "users").withConverter(userConverter);
-
-export const getInventoriesOfUser = async (user: User) => {
-  const queryDoc = query(inventories, where("owner", "==", user.email));
-
-  return getDocs(queryDoc).then((snap) => {
-    return snap.docs.map((doc) => doc.data());
-  });
-};
 
 export const isPremium = async (user: User) => {
   const queryDoc = query(users, where("email", "==", user.email));
@@ -25,4 +28,37 @@ export const isPremium = async (user: User) => {
 
     return snap.docs[0].data().premium;
   });
+};
+
+export const getInventoriesOfUser = async (user: User) => {
+  const queryDoc = query(inventories, where("owner", "==", user.email));
+
+  return getDocs(queryDoc).then((snap) => {
+    return snap.docs.map((doc) => doc.data());
+  });
+};
+
+export const createInventory = async (user: User) => {
+  const premium = await isPremium(user);
+  const maxAmount = premium
+    ? PREMIUM_INVENTORY_AMOUNT
+    : DEFAULT_INVENTORY_AMOUNT;
+  const capacity = premium
+    ? PREMIUM_INVENTORY_CAPACITY
+    : DEFAULT_INVENTORY_CAPACITY;
+
+  const amount = (await getInventoriesOfUser(user)).length;
+
+  if (amount === maxAmount) return;
+
+  return addDoc(
+    inventories,
+    new Inventory(
+      null,
+      DEFAULT_INVENTORY_NAME,
+      user.email!,
+      new Array<Item>(),
+      capacity
+    )
+  );
 };
