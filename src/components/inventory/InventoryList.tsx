935578c4d8bf3ecaa285266c034e4a10bdb462e8
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-19 00:18:20 +0200

    update InventoryList

--- src/components/inventory/InventoryList.tsx
+++ src/components/inventory/InventoryList.tsx
@@ -4,6 +4,8 @@ import { styled } from "styled-components";
 import { Inventory } from "../../entities";
 import {
   Bold,
+  Card,
+  CardCenterContent,
   CardContent,
   CardFooter,
   CardHeader,
@@ -13,9 +15,15 @@ import {
   Link,
   Paragraph,
   Subtitle,
+  Underline,
 } from "../utils";
-import { getInventoriesOfUser } from "../../config/Database";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  createInventory,
+  getInventoriesOfUser,
+  isPremium,
+} from "../../config/Database";
+import { faPenToSquare, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const ListContainer = styled.div`
   display: flex;
@@ -48,10 +56,80 @@ const InventoryCard = ({ inventory, editable = false }: InventoryCardProps) => {
       </CardContent>
       {editable && (
         <CardFooter>
-          <IconContainer icon={faPenToSquare} style={{ fontSize: "25px" }} />
+          <Subtitle>
+            <IconContainer icon={faPenToSquare} />
+          </Subtitle>
         </CardFooter>
       )}
     </ClickableCard>
+  );
+};
+
+interface InventoryExpansionProps {
+  premium: boolean;
+  inventories: number;
+}
+
+const InventoryExpansion = ({
+  premium,
+  inventories,
+}: InventoryExpansionProps) => {
+  const navigate = useNavigate();
+  const user = useContext(AuthContext);
+
+  const [loading, setLoading] = useState(false);
+
+  // Gli utenti premium hanno diritto a gestire 10 inventari piuttosto che 5
+  const maxLength = premium ? 10 : 5;
+
+  const createInventoryAndNavigate = async () => {
+    if (user === null) return;
+
+    if (loading) return;
+
+    setLoading(true);
+
+    createInventory(user)
+      .then((res) => {
+        if (res) {
+          navigate(`/inventory/${res.id}`);
+          return;
+        }
+
+        // TO-DO catch errors
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  if (inventories < maxLength) {
+    // card che punta a un nuovo inventario
+    return (
+      <ClickableCard onClick={() => createInventoryAndNavigate()}>
+        <CardCenterContent>
+          <Paragraph>Aggiungi un inventario</Paragraph>
+          <IconContainer icon={faPlus} />
+        </CardCenterContent>
+      </ClickableCard>
+    );
+  }
+
+  // Se l'utente ha raggiunto il numero massimo di inventari, viene mostrato un avviso
+  return (
+    <Card>
+      <CardCenterContent>
+        <Paragraph>
+          Hai raggiunto il numero massimo di inventari che puoi gestire
+        </Paragraph>
+        <Paragraph>
+          Scopri le funzionalità{" "}
+          <Link href="pro">
+            <Underline>premium</Underline>
+          </Link>
+        </Paragraph>
+      </CardCenterContent>
+    </Card>
   );
 };
 
@@ -68,12 +146,18 @@ const InventoryList = ({
   const [inventories, setInventories] = useState<Inventory[]>(
     new Array<Inventory>()
   );
+  const [premium, setPremium] = useState(false);
 
+  // Quando questa componente viene renderizzata, viene fatta una richiesta al database
   useEffect(() => {
-    if (user)
+    if (user) {
       getInventoriesOfUser(user).then((res) => {
         setInventories(res);
       });
+      isPremium(user).then((res) => {
+        setPremium(res);
+      });
+    }
   }, [user]);
 
   if (user)
@@ -86,6 +170,13 @@ const InventoryList = ({
             </Link>
           );
         })}
+
+        {expandable && (
+          <InventoryExpansion
+            premium={premium}
+            inventories={inventories.length}
+          />
+        )}
       </ListContainer>
     );
 
