ce2b1921895620c03cad20a471622842624ef7d7
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-26 17:35:17 +0200

    change firebase project

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8oL4xTKPP-ULaWD9fjZgKJoH8cHWK2Xk",
-  authDomain: "keep-inventory.firebaseapp.com",
-  projectId: "keep-inventory",
-  storageBucket: "keep-inventory.appspot.com",
-  messagingSenderId: "60277865116",
-  appId: "1:60277865116:web:90b5de69fd2bb65a224b59",
-  measurementId: "G-VFEJL12QFE",
+  apiKey: "AIzaSyD1doKkIxUVR0AbKD2ow7f4SARSB4GXS64",
+  authDomain: "isort-1bc55.firebaseapp.com",
+  projectId: "isort-1bc55",
+  storageBucket: "isort-1bc55.appspot.com",
+  messagingSenderId: "322330242290",
+  appId: "1:322330242290:web:88290376f2be58c42cf46b"
 };
 
 const app = initializeApp(firebaseConfig);
