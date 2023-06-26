003f182a632631e46ea28798c970ff53b0175ff8
Arcangelo Chinè <arcangelo.chine@gmail.com>
2023-06-26 17:41:28 +0200

    update Landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,84 @@
-import { Body, Header, Navbar, Title } from "../components/utils";
+import styled from "styled-components";
+import {
+  Accent,
+  Body,
+  Card,
+  CardCenterContent,
+  Content,
+  Header,
+  IconContainer,
+  Navbar,
+  Paragraph,
+  PrimaryButton,
+  Title,
+} from "../components/utils";
+
+import {
+  faBoxArchive,
+  faFileExcel,
+  faStopwatch,
+} from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+const FeaturesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+
+  & > * {
+    flex: 1;
+  }
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 // Pagina per gli ospiti
 const Landing = () => {
+  const navigate = useNavigate();
+
   return (
     <Body>
       <Navbar page="HOME" />
       <Header>
         <Title>Landing</Title>
       </Header>
+      <Content>
+        <FeaturesContainer>
+          <Card>
+            <CardCenterContent>
+              <Title>
+                <IconContainer icon={faBoxArchive} />
+              </Title>
+              <Paragraph>Gestisci i tuoi inventari</Paragraph>
+            </CardCenterContent>
+          </Card>
+          <Card>
+            <CardCenterContent>
+              <Title>
+                <IconContainer icon={faFileExcel} />
+              </Title>
+              <Paragraph>Esporta o importa le tue tabelle da Excel</Paragraph>
+              <Accent>(prossimamente)</Accent>
+            </CardCenterContent>
+          </Card>
+          <Card>
+            <CardCenterContent>
+              <Title>
+                <IconContainer icon={faStopwatch} />
+              </Title>
+              <Paragraph>
+                Salvataggio automatico per garantire l'integrità dei tuoi dati
+              </Paragraph>
+            </CardCenterContent>
+          </Card>
+        </FeaturesContainer>
+
+        <PrimaryButton onClick={() => navigate("/register")}>
+          Inizia adesso
+        </PrimaryButton>
+      </Content>
     </Body>
   );
 };
